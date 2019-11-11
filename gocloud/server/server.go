2647e4654508722916888a31ee580f775b613479
dongchenyu <34881087+dongchenyu@users.noreply.github.com>
2019-11-11 03:27:36 -0500

    Add files via upload

---
+++ gocloud/server/server.go
@@ -0,0 +1,12 @@
+package server
+import "github.com/codegangsta/martini"
+
+func NewServer(port string) {   
+    m := martini.Classic()
+
+    m.Get("/", func(params martini.Params) string {
+        return "hello world"
+    })
+
+    m.RunOnAddr(":"+port)   
+}
